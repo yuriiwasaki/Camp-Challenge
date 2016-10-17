c0bb418d45cf606f9bfaac8669366f3ea5172618
yuriiwasaki <lilyiwasaki@gmail.com>
2016-10-17 20:06:51 +0900

    php基礎2課題1の3，4

---
+++ CampChallenge_PHP/001.プログラミング基礎学習１/practice-b2-1-3.php
@@ -0,0 +1,11 @@
+<?php
+$num = 8;
+//echo $num = 8;
+
+for ($i = 1; $i < 20 ; ++$i){
+    $num * $i;
+    $sum = $num * $i;
+
+    echo $sum = $num * $i .'<br>';
+}
+?>
---
+++ CampChallenge_PHP/002.プログラミング基礎学習２/practice-b2-1-4.php
@@ -0,0 +1,8 @@
+<?php
+
+    $num = 0;
+    for ($i= 1; $i < 101 ; ++$i){
+         $num += $i;
+        echo $num.'<br>';
+    }
+?>
