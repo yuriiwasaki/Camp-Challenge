4eca173f836cb00735d2a807de5a116ccce012c1
yuriiwasaki <lilyiwasaki@gmail.com>
2016-10-18 17:38:53 +0900

    php基礎2　6

---
+++ CampChallenge_PHP/002.プログラミング基礎学習２/practice-b2-1-6.php
@@ -0,0 +1,24 @@
+<?php
+/*$num = '';
+while ($num != 'D'){
+    switch($num){
+        case 'A':
+            echo ''; $num='B'; break;
+        case 'B':
+            echo ''; $num='C'; break;
+        case 'C':
+            echo ''; $num='D'; break;
+    }
+}*/
+
+
+
+$num = 1000;
+
+while($num > 100 ){
+    $num = $num / 2;
+    echo $num.'<br>';
+}
+
+
+?>
