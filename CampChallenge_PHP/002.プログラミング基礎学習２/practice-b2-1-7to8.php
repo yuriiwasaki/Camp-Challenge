e422d49f79f8614d2da716a2e6fc4a55d729e463
yuriiwasaki <lilyiwasaki@gmail.com>
2016-10-19 17:34:31 +0900

    php基礎２課題7~9

---
+++ CampChallenge_PHP/002.プログラミング基礎学習２/practice-b2-1-7to8.php
@@ -0,0 +1,41 @@
+<?php
+
+$arr = array();
+$arr = array(0,1,2,3,4,5,6,7); //配列のインデックスは０から始まることに注意
+$arr = array('10','100','soeda','hayashi','-20','118','END');
+echo var_dump($arr).'<br>';
+
+?>
+
+<?php
+$arr = array();
+$arr = array(0,1,2,3,4,5,6,7);
+$arr = array('10','100','33','hayashi','-20','118','END');
+echo var_dump($arr);
+/* note:
+配列 大規模な変数 たくさんのデータを出し入れする箱
+
+7.要素が格納された配列を作成してください。
+順番 10, 100, 'soeda', 'hayashi', -20, 118, 'END'
+
+
+8．7で作成した配列の'soeda'を33に変更してください。*/
+
+/*$arr = array();
+$arr = array(0,1,2,3,4); //配列のインデックスは０から始まることに注意
+$arr = array('A','B','C','D','E');
+
+echo $arr[4];*/
+
+
+/*$a = 'a'; $b = 'b'; $c = 'c'; $d = 'hello!';
+$arr = array($a,$b,$c,$d);
+
+echo $arr[3];
+var_dump($arr); 文字で出したいなら変数の初期値を設定する必要があり、現状は文字列で書く必要性があまりない
+*/
+
+
+
+
+?>
---
+++ CampChallenge_PHP/002.プログラミング基礎学習２/practice-b2-1-9.php
@@ -0,0 +1,11 @@
+<?php
+/*9.以下の順で、連想配列を作成してください。
+1に'AAA', 'hello'に'world', 'soeda'に33, 20に20
+*/
+$a = array ();
+$a = array ('1' => 'AAA',
+            'hello'=> 'world',
+            'soeda' => '33',
+            "20" => "20");
+echo var_dump($a).'<br>';
+?>
