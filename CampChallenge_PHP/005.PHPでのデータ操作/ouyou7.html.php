3b4d6d2045bcbb9ac4a23da4a0a8b5f8681f7720
yuriiwasaki <lilyiwasaki@gmail.com>
2016-10-24 15:50:21 +0900

    html部分test

---
+++ CampChallenge_PHP/005.PHPでのデータ操作/ouyou7.html.php
@@ -0,0 +1,17 @@
+<html lang ="ja">
+<head>
+    <meta charset="utf-8">
+    <title>プロフィールページ</title>
+</head>
+<body>
+    <h1>Your profile</h1>
+    <p>以下の例を参考にあなたの情報を入力してください</p>
+    <td>例：
+        <li>名前:岩崎ゆり
+        <li>性別:女性
+        <li>趣味:美術鑑賞
+    </td>
+    <h2>プロフィール入力欄</h2>
+
+
+</body>
