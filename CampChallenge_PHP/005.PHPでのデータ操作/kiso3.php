c87689dc9247799eab6fa6e9c324fb084fb3ec32
yuriiwasaki <lilyiwasaki@gmail.com>
2016-11-04 13:25:26 +0900

    phpデータ操作基礎3,4

--- CampChallenge_PHP/005.PHPでのデータ操作/kiso3.php
+++ CampChallenge_PHP/005.PHPでのデータ操作/kiso3.php
@@ -1,3 +1,12 @@
 <?php
 
+//ユーザーの1回目の訪問
+$access_time = date('Y年m月d日');
+setcookie('LastLogindate',$access_time);//LastLoginDateに＄access_timeを保存
+
+$lastDate = $_COOKIE['LastLogindate'];//LastLoginDateに保存されたCookie取得
+
+echo 'お帰りなさい！○○さん！<br>';
+echo '前回ログイン日は、' . $lastDate . 'です！';
+//３．クッキーに現在時刻を記録し、次にアクセスした際に、前回記録した日時を表示してください。
 ?>
--- CampChallenge_PHP/005.PHPでのデータ操作/kiso4.php
+++ CampChallenge_PHP/005.PHPでのデータ操作/kiso4.php
@@ -1,3 +1,19 @@
 <?php
 
+
+session_start();
+$access_time = date('Y年m月d日');//1回目のアクセス
+$_SESSION['lastDate'] = $access_time;//lastDateという名前で$access_time保存
+
+echo '前回のアクセスは'. $_SESSION['lastDate'].'でした。';
+
+/*session_start();
+
+$_SESSION['message'] ='こんにちは'; $_SESSION [] の中にデータの名前
+
+echo $_SESSION['message'];*/
+
+
+
+//セッションで 現在時刻を記録し、次にアクセスした際に、前回記録した日時を表示する機能を作成
 ?>
