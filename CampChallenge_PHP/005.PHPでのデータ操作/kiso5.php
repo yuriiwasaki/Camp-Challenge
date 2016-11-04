7e3425888e19715c51ea52684e7baf915891673b
yuriiwasaki <lilyiwasaki@gmail.com>
2016-11-04 13:23:15 +0900

    phpデータ操作5,6

--- CampChallenge_PHP/005.PHPでのデータ操作/kiso5.php
+++ CampChallenge_PHP/005.PHPでのデータ操作/kiso5.php
@@ -1,3 +1,15 @@
-<?php
+<html>
+    <head>
+        <title>データ操作</title>
+    </head>
 
-?>
+    <body>
+
+        <h1>ファイルを提出</h1>
+        <form action="kiso6.php" method="post" enctype="multipart/form-data" >
+        ファイル選択：<input name="userfile" type="file"/>
+        <input type="submit" name="btnSubmit">
+        </form>
+
+    </body>
+</html>
--- CampChallenge_PHP/005.PHPでのデータ操作/kiso6.php
+++ CampChallenge_PHP/005.PHPでのデータ操作/kiso6.php
@@ -1,4 +1,17 @@
+
 <?php
+    // アップロードされたファイルの情報を確認
+    var_dump($_FILES);
+    // サーバー側に保存する名前
+    $file_name = 'upload.txt';
+    // アップロードされたファイルを移動する $_FILES [サーバに保存][一時保存のファイル][保存ファイル名]
+    move_uploaded_file($_FILES['userfile']['tmp_name'], $file_name);
 
+//５．ファイルアップロード機能を作成してください。kiso5.htmlでフォーム作成
+//６．５で作成したプログラムに、ファイルの中身を読み込んで表示する機能を追加してください。
 
+//実行するとエラー
+//Warning: move_uploaded_file(upload.txt): failed to open stream: Permission denied in /Applications/XAMPP/xamppfiles/htdocs/challenge/CampChallenge_PHP/005.PHPでのデータ操作/kiso6.php on line 8
+
+//move_uploaded_file(): Unable to move '/Applications/XAMPP/xamppfiles/temp/phpPJ1BJD' to 'upload.txt' on line 8
 ?>
